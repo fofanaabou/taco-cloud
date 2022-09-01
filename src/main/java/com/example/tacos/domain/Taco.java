54881ad8ae3649f73bd4557c8fd707f392357623
dev_afo <aboufofana76@gmail.com>
2022-09-01 23:20:23 +0200

    "initialize the project, create domain et a conroller"

---
+++ src/main/java/com/example/tacos/domain/Taco.java
@@ -0,0 +1,13 @@
+package com.example.tacos.domain;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class Taco {
+
+    private String name;
+
+    private List<Ingredient> ingredients;
+}
